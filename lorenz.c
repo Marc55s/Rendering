86f7f580326d37ea615fb5ea0b1c51d0a9c34949
marc55s <marc.schoenig@gmail.com>
2025-02-06 01:05:15 +0100

    feature: added line buffer for lorenz systems
    - A Buffer can now be used to save the locations from each iteration of
    the system
    - Added DrawFunction
    - Added UpdateFunction

--- lorenz.c
+++ lorenz.c
@@ -1,7 +1,9 @@
 #include "lorenz.h"
 #include "raylib.h"
 
-Lorenz_sys *init(double sig, double rho, double beta,double time, double x, double y, double z){
+#define POINT_AMOUNT 1000
+
+Lorenz_sys *init(double sig, double rho, double beta,double time, double x, double y, double z, int points){
     Lorenz_sys *system = (Lorenz_sys *) MemAlloc(sizeof(Lorenz_sys));
     system->x = x;
     system->y = y;
@@ -10,6 +12,11 @@ Lorenz_sys *init(double sig, double rho, double beta,double time, double x, doub
     system->rho = rho;
     system->beta = beta;
     system->time = time;
+    system->points = points;
+    system->lines = (Line3*) MemAlloc(sizeof(Line3) * points);
+
+    initLines(system, points, 1);
+
     return system;
 }
 
@@ -29,6 +36,7 @@ void progress(Lorenz_sys *system, int iterations) {
     double xt = 0;
     double yt = 0;
     double zt = 0;
+
     for (int i = 0; i < iterations; i++) {
         xt = x + t * a * (y - x);
         yt = y + t * (x * (b - z) - y);
@@ -38,4 +46,30 @@ void progress(Lorenz_sys *system, int iterations) {
     system->x = xt;
     system->y = yt;
     system->z = zt;
+}
+
+void initLines(Lorenz_sys *system, int points, int stepsBetweenPoints){
+    TraceLog(LOG_INFO, "POINTS ignored");
+    for (int i = 0; i < points; i++) {
+        Vector3 s = (Vector3){system->x,system->y,system->z};
+        progress(system, 1);
+        system->lines[i] = (Line3){s,(Vector3){system->x,system->y,system->z}};
+    }
+}
+
+void updateLines(Lorenz_sys *system){
+    for (int i = 0; i < (system->points)-1; i++) {
+        system->lines[i] = system->lines[i+1];
+    }
+    Vector3 s = (Vector3){system->x,system->y,system->z};
+    progress(system, 1);
+    system->lines[system->points-1] =(Line3){s,(Vector3){system->x,system->y,system->z}}; 
+}
+
+void DrawLorenzSystem(Lorenz_sys *system, int points, Color color){
+    Line3 *lines = system->lines;
+    for (int i = 0; i < system->points; i++) {
+        Line3 line = lines[i];
+        DrawLine3D(line.start, line.end, color);
+    }
 }
--- lorenz.h
+++ lorenz.h
@@ -1,11 +1,24 @@
+#include "raylib.h"
+#define BUF_SIZE 1000
+
 //TODO: vectorize struct?
-typedef struct lorenz_system{
+
+typedef struct Line3 {
+    Vector3 start;
+    Vector3 end;
+} Line3;
+
+typedef struct lorenz_system {
     double x,y,z; 
     double sig,rho,beta;
     double time;
+    int points;
+    Line3 *lines;
 } Lorenz_sys;
 
-Lorenz_sys *init(double sig, double rho, double beta,double time, double x, double y, double z);
+Lorenz_sys *init(double sig, double rho, double beta,double time, double x, double y, double z, int points);
 
 void progress(Lorenz_sys *system, int iterations);
-
+void DrawLorenzSystem(Lorenz_sys *system, int points, Color color);
+void initLines(Lorenz_sys *system, int points, int stepsBetweenPoints);
+void updateLines(Lorenz_sys *system);
